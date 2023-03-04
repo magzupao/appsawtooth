caf1530274d108a48a24e327e4c326e0be237afc
magzupao <mguado@gmail.com>
2023-03-04 22:57:36 +0100

    test

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { TransactionProcessor } = require('sawtooth-sdk-js/processor')
+const IntegerKeyHandler = require('./handler/inckey')
 
 const VALIDATOR_URL = 'tcp://localhost:4004'
 const transactionProcessor = new TransactionProcessor(VALIDATOR_URL)
+
+transactionProcessor.addHandler(new IntegerKeyHandler());
 
 transactionProcessor.start();
 
 process.on('SIGUSR2', () => {
   transactionProcessor._handleShutdown();
 })
-
 
 console.log('hola!!!!!!!')
