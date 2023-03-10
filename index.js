0c2017ee95eae6de220ab65a51d5a747e2d6b16a
magzupao <mguado@gmail.com>
2023-03-10 15:01:42 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hola magz !!!!!!!')
+const { TransactionProcessor } = require('sawtooth-sdk-js/processor')
+
+const VALIDATOR_URL = 'tcp://localhost:4004'
+const transactionProcessor = new TransactionProcessor(VALIDATOR_URL)
+
+transactionProcessor.start();
+console.log('hola magz 1 !!!!!!!')
