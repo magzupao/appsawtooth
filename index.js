bc8f76b4262045bf9bfda895f95fb277e19776da
magzupao <mguado@gmail.com>
2023-03-10 15:07:50 -0500

    test

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const VALIDATOR_URL = 'tcp://localhost:4004'
 const transactionProcessor = new TransactionProcessor(VALIDATOR_URL)
 
 transactionProcessor.start();
+
+process.on('SIGUSR2', () => {
+  transactionProcessor._handleShutdown();
+})
+
 console.log('hola magz 1 !!!!!!!')
